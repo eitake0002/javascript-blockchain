f99e5fc47dcb48a7aad0fd9f07997700e73f8c84
eitake0002 <eiki.e.t.d@gmail.com>
2018-08-24 18:25:12 +0900

    commit for all

---
+++ add_nodes.sh
@@ -0,0 +1,1 @@
+curl -d '{"newNodeUrl": "http://localhost:3005"}' -X POST http://localhost:3001/register-and-broadcast-node
--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,8 +1,14 @@
 const sha256 = require('sha256');
+const currentNodeUrl = process.argv[3]
+const uuid = require('uuid/v1');
+
 
 function Blockchain() {
     this.chain = [];
     this.pendingTransactions = [];
+    
+    this.currentNodeUrl = currentNodeUrl;
+    this.networkNodes = [];
     
     this.createNewBlock(100, '0', '0');
 }
@@ -28,16 +34,20 @@ Blockchain.prototype.getLastBlock = function() {
 }
 
 Blockchain.prototype.createNewTransactions = function(amount, sender, recipient) {
-    const newTransactions = {
+    const newTransaction = {
         amount: amount,
         sender: sender,
-        recipient: recipient
+        recipient: recipient,
+        transactionId: uuid().split('-').join('')
     };
     
-    this.pendingTransactions.push(newTransactions);
-    
+    return newTransaction;
+};
+
+Blockchain.prototype.addTransactionToPendingTransactions = function(transactionObj){
+    this.pendingTransactions.push(transactionObj);
     return this.getLastBlock()['index'] + 1;
-}
+};
 
 Blockchain.prototype.hashBlock = function(previousBlockHash, currentBlockData, nonce) {
     const dataAsString = previousBlockHash + nonce.toString() + JSON.stringify(currentBlockData);
@@ -56,5 +66,41 @@ Blockchain.prototype.proofOfWork = function(previousBlockHash, currentBlockData)
     
     return nonce;
 }
+
+Blockchain.prototype.chainIsValid = function(blockchain){
+
+    let validChain = true;
+    
+    for (var i = 1; i < blockchain.length; i++) {
+        const currentBlock = blockchain[i];
+        const prevBlock = blockchain[i -1];
+        const blockHash = this.hashBlock(prevBlock['hash'], {transactions: currentBlock['transactions'], index: currentBlock['index']}, currentBlock['nonce']);
+
+        if (blockHash.substring(0, 4) != '0000') validChain = false;
+        if (currentBlock['previousBlockHash'] !== prevBlock) validChain = false;
+        
+        console.log('previousBlockHash =>', prevBlock['hash']);
+        console.log('currentBlockHash =>', currentBlock['hash'])
+    };
+    
+    const genesysBlock = blockchain[0];
+    const correctNonce = genesysBlock['none'] == 100;
+    const correctPreviousBlock = genesysBlock['previousBlockHash'] === '0';
+    const correctHash = genesysBlock['hash'] === '0';
+    const correctTransactions = genesysBlock['transactions'].length === 0;
+
+    if (!correctNonce || !correctPreviousBlock || !correctHash || !correctTransactions) validChain = false;
+    
+    return validChain;
+};
+
+Blockchain.prototype.getBlock = function(blockHash) {
+    let correctBlock = null;
+    this.chain.forEach(block => {
+        if (block.hash === blockHash) correctBlock = block;
+    });
+    
+    return correctBlock;
+};
 
 module.exports = Blockchain;
